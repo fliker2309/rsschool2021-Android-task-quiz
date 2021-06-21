accd5ecdc45a3e118e20b87fea2485362c9e9d85
Evgenii <60140529+fliker2309@users.noreply.github.com>
2021-06-21 21:12:25 +0300

    fix: navController on 1st question

--- app/src/main/java/com/rsschool/quiz/ResultFragment.kt
+++ app/src/main/java/com/rsschool/quiz/ResultFragment.kt
@@ -7,7 +7,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import com.rsschool.quiz.data.Constants
 import com.rsschool.quiz.databinding.FragmentResultBinding
